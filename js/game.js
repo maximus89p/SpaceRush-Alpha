e55f746d8df8b79228d088bea2993bf7d5a170ad
maximus89p <maximuspras@gmail.com>
2016-02-05 14:19:21 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,31 @@
-
+//this game will have only 1 state
+ 
+var GameState = {
+ 
+  //load the game assets before the game starts
+ 
+  preload: function() {
+ 
+  },
+ 
+  //executed after everything is loaded
+ 
+  create: function() {
+ 
+  },
+ 
+  //game loop, executed many times per second
+ 
+  update: function() {
+ 
+  }
+ 
+};
+ 
+//initiate the Phaser framework
+ 
+var game = new Phaser.Game(360, 640, Phaser.AUTO);
+ 
+game.state.add('GameState', GameState);
+ 
+game.state.start('GameState');
